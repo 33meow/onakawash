4e1a252367c496f7c2a0aad6a16b6e3034f1d4c4
Buttercup <mayiyuanchenze@gmail.com>
2026-06-25 21:21:26 +0800

    refactor: remove legacy intro sidebar after SideNav migration (迁移到 SideNav 后删除介绍页旧侧栏)

--- app/intro/page.tsx
+++ app/intro/page.tsx
@@ -45,127 +45,14 @@ const t = messages[language];
         alignItems: "stretch",
       }}
     >
-     <SideNav currentPage="intro" language={language}   setLanguage={setLanguage}/>
+     
+      <SideNav
+      currentPage="intro"
+      language={language}
+      setLanguage={setLanguage}
+    />
       
-        {/* 左侧导航栏：下一步会放入主页按钮和三个学习入口 */}
-        <nav 
-            aria-label="介绍页导航"
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "stretch",
-                gap: "16px",
-                padding: "16px 12px",
-                borderRight: "1px solid #ead8d0",
-            }}>
-              {/* 导航栏第一个入口：返回主页 */}
-            <HomeButton label={t.nav.home} />
-             {/* 导航入口 2：进入平假名页面 */}
-            <Link
-                href="hiragana"
-                className="side-nav-item"
-               >
-                  <Image
-                  className="side-nav-image"
-                      src="/images/buttons/hiragana.png"
-                      alt=""
-                      width={72}
-                      height={72}
-                      />
-                      <span>{t.nav.hiragana}</span>
-                </Link>
-
-         
-
-            {/* 导航入口 3：进入片假名页面 */}
-<Link
-  href="/katakana"
-  className="side-nav-item"
- 
->
-  <Image
-  className="side-nav-image"
-    src="/images/buttons/katakana.png"
-    alt=""
-    width={72}
-    height={72}
-  />
-
-  <span>{t.nav.katakana}</span>
-</Link>
-{/* 导航入口 4：尚未开放的汉字功能 */}
-<button
-  type="button"
-  disabled
-  style={{
-    display: "flex",
-    alignItems: "center",
-    gap: "8px",
-    padding: "0",
-    border: "none",
-    background: "none",
-    color: "#8f817b",
-    font: "inherit",
-    fontWeight: "700",
-    opacity: "0.6",
-    textAlign: "left",
-      cursor: "not-allowed",
-  }}
->
-  <Image
-    src="/images/buttons/kanji.png"
-    alt=""
-    width={72}
-    height={72}
-  />
-
-  <span  style={{
-    flex: "1",
-    textAlign: "left",
-    lineHeight: "1.4",
-  }}>{t.nav.kanjiComingSoon}</span>
-</button>
-{/* 导航入口 5：尚未开放的设置 */}
-<button
-  type="button"
-  disabled
-  style={{
-    width: "100%",
-    display: "flex",
-    alignItems: "center",
-    gap: "8px",
-    padding: "0",
-    border: "none",
-    background: "transparent",
-    color: "#8f817b",
-    font: "inherit",
-    fontWeight: "700",
-    opacity: "0.6",
-    cursor: "not-allowed",
-    textAlign: "left",
-  }}
->
-  <Image
-    src="/images/buttons/settings.png"
-    alt=""
-    width={72}
-    height={72}
-  />
-
-  <span
-    style={{
-      flex: "1",
-      textAlign: "left",
-      lineHeight: "1.4",
-    }}
-  >
-    {t.nav.settings}
-  </span>
-</button>
-{/* 导航入口 6：切换语言 */}
-<LanguageMenu language={language}
-setLanguage={setLanguage}/>
-            </nav>
+       
    <article
   style={{
     width: "100%",
